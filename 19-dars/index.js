40415b39c6050f050466ef7b31797e1d6df21416
Furqatjon2012 <furqatjon200@gmail.com>
2025-05-19 15:59:46 +0300

    java-scirpt

---
+++ 19-dars/index.js
@@ -0,0 +1,32 @@
+//                                  1-vazifa
+
+//let arr = [
+//    {id: 1, name: "Eshamt", age: 20},
+//    {id: 2, name: "Toshmat", age: 30},
+//    {id: 3, name: "Gulmat", age: 12},
+//    {id: 4, name: "Gulbashaka", age: 17},
+//    {id: 5, name: "Eshamtjon", age: 35}
+//]
+
+//arr.sort((a, b) => a.age - b.age);
+
+//console.log(arr);
+
+//                                  2-vazifa
+
+//let arr2 = [
+//    {id: 1, name: "Eshmat", status: "Talaba"},
+//    {id: 2, name: "Toshmat", status: "Ishchi"},
+//    {id: 3, name: "Gulmat", status: "Oqituvchi"},
+//    {id: 4, name: "Gulbashaka", status: "Bekorchi"},
+//];
+
+//let search = prompt("Harf kiriting:");
+
+//let result = arr2.filter(obj => obj.status.toLowerCase().includes(search.toLowerCase()));
+
+//console.log(result);
+
+//                                  3-vazifa
+
+let 
