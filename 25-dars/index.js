00500eba57bd644f314cd4874dae9b917729591e
Furqatjon2012 <furqatjon200@gmail.com>
2025-06-11 09:40:50 +0300

    first

--- 25-dars/index.js
+++ 25-dars/index.js
@@ -4,6 +4,8 @@
 //let result = a.filter(item => !b.includes(item));
 //console.log(result);
 
+//===================================================================
+
 //let arr = [5, 8, 6, 3, 4];
 
 //let juft = [];
@@ -29,6 +31,8 @@
 
 //console.log(newArr);
 
+//===================================================================
+
 //let arr = [1, 1, 2, 3, 4, 4, 5, 6, 6];
 //let newArr = arr.filter((value, index) => {
 //    if (arr.indexOf(value) === arr.lastIndexOf(value)) return value;
@@ -36,3 +40,38 @@
 
 //console.log(newArr);
 
+//===================================================================
+
+//let a = ["a", "b", "d", "e", "f"];
+//let b = ["a", "b", "c", "d", "e", "f"]
+
+//let arr = [];
+
+//if (a.length < b.length) {
+//    for (let value of b) {
+//        if (!a.includes(value)) arr.push(value);
+//    }
+//}else{
+//    for (let value of a) {
+//        if (!b.includes(value)) arr.push(value);
+//    }
+//}
+
+//console.log(arr);
+
+//===================================================================
+
+let str = "Tez jigarrang tulki dangasa it ustidan sakrab o'tadi"
+
+let arr = str.split(" ");
+let newArr = [];
+arr.forEach((value) => {
+    for (let letter of value) {
+        if (value.indexOf(letter) === value.lastIndexOf(letter)) newStr += letter;
+        else {
+            newStr = "";
+            break;
+        }
+    }
+    if (newStr.lenght) newArr.push(newStr);
+})
